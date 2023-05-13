5684f7d278db8d63eddcbb507f5595c85b758d36
exe <igorwdowiczak@gmail.com>
2023-05-13 17:19:06 +0200

    :anchor: Layout, HTML Theme added

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,24 +1,68 @@
-<h1>{{$heading}}</h1>
+@extends('layout')
+
+@section('content')
 
 
-{{-- @if(count($listings) == 0)
-    <p>No listings found</p>
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
-@endif --}}
 
-@unless(count($listings) == 0) 
+        {{-- @if(count($listings) == 0)
+            <p>No listings found</p>
 
-    @foreach($listings as $listing)
-        <h2>
-            <a href ="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
-        </h2>
-        <p>
-            {{$listing['description']}}
-        </p>
+        @endif --}}
 
-    @endforeach
+        @unless(count($listings) == 0) 
 
-@else 
-    <p>No listings found</p>
+            @foreach($listings as $listing)
 
-@endunless
+                <div class="bg-gray-50 border border-gray-200 rounded p-6">
+                    <div class="flex">
+                        <img
+                            class="hidden w-48 mr-6 md:block"
+                            src="{{asset('images/no-image.png')}}"
+                            alt=""
+                        />
+                        <div>
+                            <h3 class="text-2xl">
+                                <a href="show.html">{{$listing->title}}</a>
+                            </h3>
+                            <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+                            <ul class="flex">
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">Laravel</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">API</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">Backend</a>
+                                </li>
+                                <li
+                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                                >
+                                    <a href="#">Vue</a>
+                                </li>
+                            </ul>
+                            <div class="text-lg mt-4">
+                                <i class="fa-solid fa-location-dot"></i> {{$listing->location}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            @endforeach
+
+        @else 
+            <p>No listings found</p>
+
+        @endunless
+
+    </div>
+
+@endsection
