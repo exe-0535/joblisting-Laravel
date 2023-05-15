2366f2c72fd77eff668ad8bf4c90200aa48111f7
exe <igorwdowiczak@gmail.com>
2023-05-15 12:57:17 +0200

    :anchor: Update listing

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,14 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 
 Route::post('/listings', [ListingController::class, 'store']);
 
+// Show Edit Form
+
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update Listing
+
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 // Single Listing
 
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
