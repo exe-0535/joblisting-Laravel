e424a0620f768159b1cade7a71aedb96b2c2a6f3
exe <igorwdowiczak@gmail.com>
2023-05-29 13:57:11 +0200

    :anchor: Modified searching form - added fields for location and searching scope

--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,23 +1,51 @@
 <form action="/">
-    <div class="relative border-2 border-gray-100 m-4 rounded-lg">
-        <div class="absolute top-4 left-3">
-            <i
-                class="fa fa-search text-gray-400 z-20 hover:text-gray-500"
-            ></i>
+    <div class="relative border-2 border-gray-100 m-4 rounded-lg flex">
+        <div class="relative w-1/5 mr-4">
+            <div class="absolute top-4 left-3">
+                <i
+                    class="fa fa-location-arrow text-gray-400 z-20 hover:text-gray-500"
+                ></i>
+            </div>
+            <input
+                type="text"
+                name="location"
+                class="h-14 pl-10 w-full rounded-lg z-0 focus:shadow focus:outline-none"
+                placeholder="Location, fe. Warsaw, Tokyo"
+            />
         </div>
-        <input
-            type="text"
-            name="search"
-            class="h-14 w-full pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-            placeholder="Search Laravel Gigs..."
-        />
-        <div class="absolute top-2 right-2">
-            <button
-                type="submit"
-                class="h-10 w-20 text-white rounded-lg bg-red-500 hover:bg-red-600"
-            >
-                Search
-            </button>
+        <div class="relative w-1/5 mr-4">
+            <div class="absolute top-4 left-3">
+                <i
+                    class="fa fa-area-chart text-gray-400 z-20 hover:text-gray-500"
+                ></i>
+            </div>
+            <input
+                type="number"
+                name="range"
+                class="h-14 pl-10 w-full rounded-lg z-0 focus:shadow focus:outline-none"
+                placeholder="Search scope in km, fe. 20"
+            />
+        </div>
+        <div class="relative w-full">
+            <div class="absolute top-4 left-3">
+                <i
+                    class="fa fa-search text-gray-400 z-20 hover:text-gray-500"
+                ></i>
+            </div>
+            <input
+                type="text"
+                name="search"
+                class="h-14 pl-10 w-full pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+                placeholder="Search listings by keywords..."
+            />
+            <div class="absolute top-2 right-2">
+                <button
+                    type="submit"
+                    class="h-10 w-20 text-white rounded-lg bg-red-500 hover:bg-red-600"
+                >
+                    Search
+                </button>
+            </div>
         </div>
     </div>
 </form>
