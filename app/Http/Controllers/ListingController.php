976173c6a0fefea917e6382292d0d11d633e7496
exe <igorwdowiczak@gmail.com>
2023-05-29 14:44:09 +0200

    :anchor: Implemented location search with proper validation

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -13,6 +13,42 @@ class ListingController extends Controller
 {
     // Show all listings
     public function index() {
+
+        if(strlen(request('location')) > 0) {
+
+            $apiresponse = Http::get('https://nominatim.openstreetmap.org/search?q=' . request('location') . '&format=json&limit=1')->object();
+
+            if(count($apiresponse) < 1) {
+                return redirect()->back()->withErrors(['location' => 'Please enter a valid location'])->withInput();
+            }
+
+            $lat = $apiresponse[0]->lat;
+            $lon = $apiresponse[0]->lon;
+
+            if(is_null(request()->range)) {
+                $range = 10;
+            } else {
+                $range = request()->range;
+                if($range < 10 || $range > 250) {
+                    return redirect()->back()->withErrors(['range' => 'Please enter a valid range'])->withInput();
+                }
+            }
+            
+            $haversine = "(
+                6371 * acos(
+                    cos(radians(" .$lat. "))
+                    * cos(radians(`latitude`))
+                    * cos(radians(`longitude`) - radians(" .$lon. "))
+                    + sin(radians(" .$lat. ")) * sin(radians(`latitude`))
+                )
+            )";
+
+            return view('listings.index', [
+                'listings' => Listing::selectRaw("*, $haversine AS distance")->latest()->having("distance", "<=", $range)->paginate(6)
+            ]);
+
+        }
+
         return view('listings.index', [
             'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
--- resources/views/partials/_search.blade.php
+++ resources/views/partials/_search.blade.php
@@ -1,6 +1,6 @@
 <form action="/">
     <div class="relative border-2 border-gray-100 m-4 rounded-lg flex">
-        <div class="relative w-1/5 mr-4">
+        <div class="relative w-1/4 mr-4">
             <div class="absolute top-4 left-3">
                 <i
                     class="fa fa-location-arrow text-gray-400 z-20 hover:text-gray-500"
@@ -12,8 +12,11 @@
                 class="h-14 pl-10 w-full rounded-lg z-0 focus:shadow focus:outline-none"
                 placeholder="Location, fe. Warsaw, Tokyo"
             />
+            @error('location')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
-        <div class="relative w-1/5 mr-4">
+        <div class="relative w-1/3 mr-4">
             <div class="absolute top-4 left-3">
                 <i
                     class="fa fa-area-chart text-gray-400 z-20 hover:text-gray-500"
@@ -23,8 +26,11 @@
                 type="number"
                 name="range"
                 class="h-14 pl-10 w-full rounded-lg z-0 focus:shadow focus:outline-none"
-                placeholder="Search scope in km, fe. 20"
+                placeholder="Search scope in km (from 10 to 250)"
             />
+            @error('range')
+                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
         <div class="relative w-full">
             <div class="absolute top-4 left-3">
