af5988dc54015df0dbf4b916d48fb81b84d8d27c
exe <igorwdowiczak@gmail.com>
2023-06-01 12:28:56 +0200

    :anchor: Listings don't have no 'tags' field anymore

--- database/migrations/2023_05_13_143845_create_listings_table.php
+++ database/migrations/2023_05_13_143845_create_listings_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->string('logo')->nullable();
-            $table->string('tags');
             $table->string('company');
             $table->string('location');
             $table->decimal('latitude', 18, 15);
