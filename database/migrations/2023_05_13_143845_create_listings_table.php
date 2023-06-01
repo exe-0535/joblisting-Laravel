a2e968b7957db446f3c919d999757e6d8a521fba
exe <igorwdowiczak@gmail.com>
2023-06-01 14:21:47 +0200

    :anchor: Fixed migration bugs

--- database/migrations/2023_05_13_143845_create_listings_table.php
+++ database/migrations/2023_05_13_143845_create_listings_table.php
@@ -32,6 +32,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('listings');
+        Schema::enableForeignKeyConstraints();
     }
 };
